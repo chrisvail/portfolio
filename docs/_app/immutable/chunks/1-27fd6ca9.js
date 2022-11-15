cd58a318b34f7e7072394b85ae36181b4c03bd2f
Chris Vail <christopher.n.vail@gmail.com>
2022-11-15 18:01:40 +0000

    Changed background colour

--- docs/_app/immutable/chunks/1-27fd6ca9.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_error.svelte-dd75b142.js";export{t as component};
---
+++ docs/_app/immutable/chunks/1-ca977617.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_error.svelte-1952adae.js";export{t as component};
