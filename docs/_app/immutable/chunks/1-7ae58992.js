e91f2fca52759d434a93d0d1a6a603c598361677
chrisvail <christopher.n.vail@gmail.com>
2023-09-02 17:24:04 +0100

    Updated CV

---
+++ docs/_app/immutable/chunks/1-7ae58992.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_error.svelte-006c6f00.js";export{t as component};
---
+++ docs/_app/immutable/chunks/2-857f1036.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-6f614873.js";export{t as component};
